d1f0e1538a257f188526abde86e0729a288fb8e1
Student <venus.kabera@student.moringaschool.com>
2022-11-18 21:17:37 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+function whisper(string)  {
+    return string.toLowerCase()
+}
+
+function logShout (string) {
+    console.log(string.toUpperCase( ));
+  }
+  
+  function logWhisper (string) {
+    console.log(string.toLowerCase( ));
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    let yeah = "i woul love to!";
+   
+    if (string.toLowerCase(string) === string) {
+     return "I can't hear you!";
+    }
+    else if (string.toUpperCase(string) === string) {
+      return"YES INDEED!";
+    }
+    else {return "I would love to!";
+    }
+  }
